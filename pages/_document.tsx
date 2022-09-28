251da29df3d7e7bdd9edca74f69a529727432d6c
Mangpha <8350130@gmail.com>
2022-09-28 17:08:39 +0900

    # Init / Install Dependencies

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="ko">
+				<Head>
+					<meta charSet="utf-8" />
+					<Script
+						src="https://kit.fontawesome.com/7e5e711513.js"
+						crossOrigin="anonymous"
+					></Script>
+					<meta name="theme-color" content="#000000" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
