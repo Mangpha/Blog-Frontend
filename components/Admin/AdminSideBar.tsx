8a3350d2bd31e8aba49e3a765da246d2f13fcf75
Mangpha <8350130@gmail.com>
2022-10-03 07:59:14 +0900

    # Feature / Admin Category Panel

--- components/Admin/AdminSideBar.tsx
+++ components/Admin/AdminSideBar.tsx
@@ -8,7 +8,10 @@ export const AdminSideBar = () => {
       <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-900">
         <ul className="space-y-2">
           <li>
-            <div className="flex cursor-pointer items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <div
+              onClick={() => router.push('/admin')}
+              className="flex cursor-pointer items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
               <svg
                 aria-hidden="true"
                 className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
