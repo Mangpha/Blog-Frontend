b81923480e2783d87edeb49f0cd8e3be3fffe812
Mangpha <8350130@gmail.com>
2022-10-20 11:47:45 +0900

    # Fix / fix comment loading error

--- components/Blog/Comments.tsx
+++ components/Blog/Comments.tsx
@@ -1,5 +1,20 @@
 const Comment = () => {
-  return (
+  return typeof window !== 'undefined' && localStorage.getItem('theme') === 'undefined' ? (
+    <section
+      className="my-5 utterance-light"
+      ref={(el) => {
+        if (!el) return;
+        const scriptEl = document.createElement('script');
+        scriptEl.src = 'https://utteranc.es/client.js';
+        scriptEl.async = true;
+        scriptEl.setAttribute('repo', 'Mangpha/blog-comments');
+        scriptEl.setAttribute('issue-term', 'pathname');
+        scriptEl.setAttribute('crossorigin', 'anonymous');
+        scriptEl.setAttribute('theme', 'github-light');
+        el.appendChild(scriptEl);
+      }}
+    />
+  ) : (
     <>
       <section
         className="my-5 utterance-light"
