3a810c44941f900042a448c682824d13dfb4203d
Mangpha <8350130@gmail.com>
2022-10-20 15:44:50 +0900

    # Fix / Post Card UI fix

--- components/Blog/PostCard.tsx
+++ components/Blog/PostCard.tsx
@@ -1,18 +1,36 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
+import { FindPostsQuery_findAllPosts_posts_category } from '../../pages/api/__graphql__/FindPostsQuery';
 
 interface IPostCardProps {
-  category: string;
+  id: number;
+  category: FindPostsQuery_findAllPosts_posts_category | null;
   title: string;
   createdAt: string;
 }
 
-const PostCard: NextPage<IPostCardProps> = ({ category, title, createdAt }) => {
+const PostCard: NextPage<IPostCardProps> = ({ category, title, createdAt, id }) => {
   return (
-    <div className="py-10 px-5 rounded overflow-hidden shadow-xl scale-90 hover:scale-100 transition-transform duration-200">
-      <p className="text-md mb-3 text-gray-500">{category}</p>
-      <p className="text-2xl mb-5 text-ellipsis">{title}</p>
-      <div className="flex justify-end">
-        <span className="text-md text-gray-500">{createdAt}</span>
+    <div className="flex justify-center">
+      <div className="block rounded-lg shadow-lg text-center w-full">
+        <div className="py-3 px-6 border-b border-gray-300 text-gray-500">
+          {category?.id ? (
+            <Link href={`/blog/category/${category?.id}`}>
+              <a>{category.name}</a>
+            </Link>
+          ) : (
+            '-'
+          )}
+        </div>
+        <div className="p-6 h-36">
+          <div className="text-xl break-words">{title}</div>
+        </div>
+        <Link href={`/blog/${id}`}>
+          <div className="inline-flex justify-center rounded-md border border-transparent hover:cursor-pointer mb-5 bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+            Read More
+          </div>
+        </Link>
+        <div className="py-3 px-6 border-t border-gray-300 text-gray-500">{createdAt}</div>
       </div>
     </div>
   );
